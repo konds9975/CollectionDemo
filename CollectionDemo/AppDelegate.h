f315f4e4c98a87c9d7937b2a90ff7cf2ed517db9
Kondya <kondya@apples-macbook-air.local>
2018-06-08 22:23:18 +0530

    Initial Commit

---
+++ CollectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionDemo
+//
+//  Created by Kondya on 08/06/18.
+//  Copyright © 2018 Kondya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionDemo
+//
+//  Created by Kondya on 08/06/18.
+//  Copyright © 2018 Kondya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
